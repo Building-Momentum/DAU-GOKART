4ec937221e649f06a0aa683a8aa517a1247d3e89
MoisesBuildMo <moises@buildmo.com>
2024-09-09 14:22:54 -0400

    Create goKart_Deadman_CLEANED.ino

---
+++ Code/goKart_Deadman/goKart_Deadman_CLEANED/goKart_Deadman_CLEANED.ino
@@ -0,0 +1,46 @@
+/*
+  Refined code for DAU Go Kart project. This code is for the deadman portion of the Go Kart.
+
+    -Place all Serial comms within an #ifdef/#endif pair.
+    -Use defines for all non-changing values
+    -Use all caps for all #defines
+
+  Use a Mega for development
+
+  Contriubutors:
+    M. Pena
+
+
+*/
+
+// Declarations for use
+#define DEADMAN_PIN_IN 11  //These variables are setup for the Deadman switch and braking operation of the go kart
+
+
+// Included Libraries
+
+
+// Global Classes/Stuctures
+
+
+// Global variables
+int deadmanValue = 0;
+
+
+void setup() {
+
+  Serial.begin(9600);  // Allows for the Serial Monitor to be used
+
+  pinMode(DEADMAN_PIN_IN, INPUT);
+
+}
+
+void loop() {
+  deadmanValue = pulseIn(DEADMAN_PIN_IN, HIGH);
+
+
+  while (deadmanValue > 1600) {
+    Serial.print("Deadman switch is pressed!");
+    deadmanValue = pulseIn(DEADMAN_PIN_IN, HIGH);
+  }
+}
